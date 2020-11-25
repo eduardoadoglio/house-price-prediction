6b65fbf129c63c3b04e351e0dc62233d3b97a3b3
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-11-25 18:04:57 -0300

    Remove static paths

--- script.r
+++ script.r
@@ -4,28 +4,26 @@ library(dplyr)
 library(scales)
 library(psych)
 
-install.packages("psych")
-
-setwd("C:/Users/eduar/Downloads/EACH EAD/20202/MQA/segundo_trabalho")
+install.packages("")
 
 # Raw house data
-data <- read.csv("kc_house_data.csv", header = TRUE)
+data <- read.csv(file.choose(), header = TRUE)
 
 # Get 5000 rows sample from original data
 sampled_data <- data[sample(nrow(data), 5000),]
 # Remove unwanted columns
 sampled_data <- sampled_data[, c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 21)]
 
-
 # Get correlation matrix
 cor(sampled_data$price, sampled_data$sqft_living)
-# Multiple linear regression
 
+# Multiple linear regression
 fit <- lm(price ~ sqft_living + waterfront + view + condition + grade + sqft_living15, data=my_data)
 
 fit <- lm(price ~ sqft_living, data=my_data)
 
 fit <- lm(price ~ ., data=sampled_data)
 
+# Summary of multiple linear regression results
 summary(fit)
 
