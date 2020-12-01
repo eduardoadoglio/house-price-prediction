f2cf7d5b9f4190333b7c80a671e0e92ab41d9230
eduardoadoglio <eduardoadoglio4@gmail.com>
2020-11-30 23:26:03 -0300

    Remove duplicate and formatting

--- main.r
+++ main.r
@@ -4,6 +4,45 @@ library(dplyr)
 library(scales)
 library(psych)
 
+#install.packages("reshape2")
+
+# Remove scientific notation
+options(scipen=999)
+
+# Sets working directory to current directory, if you're using RStudio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# House data
+data <- read.csv(file.choose(), header = TRUE)
+# Get 5000 rows sample from original data
+reduced_data <- data[sample(nrow(data), 5000),]
+# Defining sample size
+sample_size <- floor(0.70 * nrow(reduced_data))
+# Set Seed so that same sample can be reproduced in future also
+# Always set the seed before sampling
+set.seed(101)
+# Now Selecting 70% of data as sample from 5000 rows of the data
+train_ind <- sample(seq_len(nrow(data)), size = sample_size)
+train <- data[train_ind, ]
+test <- data[-train_ind, ]
+#Generating a new column with data that indicates how much time has passed since the 
+#last renovation, i.e. cur_year - yr_renovated (if the house has never been renovated,
+#yr_built replaces yr_renovated in the formula).
+data$lst_renovation <- 2020 - data$yr_renovated
+data$lst_renovation <- ifelse(data$lst_renovation == 2020, 2020 - data$yr_built, data$lst_renovation)
+
+#Generating a new column with data to replace the basement area with a variable
+#that indicates a basement existence 
+data$has_basement <- ifelse(data$sqft_basement >= 1, 1, 0)
+
+#Remove unwanted columns
+data <- data[c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 20, 21, 22, 23)]
+#Creating a dataset copy to plot correlation matrix without yr_build.
+dataCor <- data[c(1, 2, 3, 4, 5, 6, 8 ,9 ,10, 11, 12, 13, 14, 15, 16)]
+#Calculating houses' age
+dataCor$age <- 2020 - dataCor$yr_built
+#Deleting useless columns
+dataCor <- dataCor[c(1, 2, 3, 4, 5, 6, 8 ,9 ,10, 12, 13, 14, 15, 16)]
+
 # Helper functions
 # Get lower triangle of the correlation matrix
 get_lower_tri<-function(cor_mat){
@@ -194,44 +233,8 @@ grade_boxplot <- function(data) {
     )
 }
 
-#install.packages("reshape2")
 
-# Remove scientific notation
-options(scipen=999)
-
-# Sets working directory to current directory, if you're using RStudio
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-# House data
-data <- read.csv(file.choose(), header = TRUE)
-# Get 5000 rows sample from original data
-reduced_data <- data[sample(nrow(data), 5000),]
-# Defining sample size
-sample_size <- floor(0.70 * nrow(reduced_data))
-# Set Seed so that same sample can be reproduced in future also
-# Always set the seed before sampling
-set.seed(101)
-# Now Selecting 70% of data as sample from 5000 rows of the data
-train_ind <- sample(seq_len(nrow(data)), size = sample_size)
-train <- data[train_ind, ]
-test <- data[-train_ind, ]
-#Generating a new column with data that indicates how much time has passed since the 
-#last renovation, i.e. cur_year - yr_renovated (if the house has never been renovated,
-#yr_built replaces yr_renovated in the formula).
-data$lst_renovation <- 2020 - data$yr_renovated
-data$lst_renovation <- ifelse(data$lst_renovation == 2020, 2020 - data$yr_built, data$lst_renovation)
-
-#Generating a new column with data to replace the basement area with a variable
-#that indicates a basement existence 
-data$has_basement <- ifelse(data$sqft_basement >= 1, 1, 0)
-
-#Remove unwanted columns
-data <- data[c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 20, 21, 22, 23)]
-#Creating a dataset copy to plot correlation matrix without yr_build.
-dataCor <- data[c(1, 2, 3, 4, 5, 6, 8 ,9 ,10, 11, 12, 13, 14, 15, 16)]
-#Calculating houses' age
-dataCor$age <- 2020 - dataCor$yr_built
-#Deleting useless columns
-dataCor <- dataCor[c(1, 2, 3, 4, 5, 6, 8 ,9 ,10, 12, 13, 14, 15, 16)]
+# Exploratory analysis function calls
 
 # Scatter plots
 generate_price_by_sqft_living_scatter_plot(data)
@@ -240,21 +243,19 @@ generate_beeswarm_plus_boxplot_for_price_and_grade(data)
 generate_price_by_bedroom_scatter_plot(data)
 generate_price_by_bathroom_scatter_plot(data)
 generate_price_by_waterfront_scatter_plot(data)
+
+# Histograms
 grade_histogram(data)
 sqft_histogram(data)
+
+# Boxplots
 waterfront_boxplot(data)
 grade_boxplot(data)
+
 #Correlation
 generate_lower_triangle_correlation_matrix_heatmap(dataCor)
-#Defining sample size
-sample_size <- floor(0.70 * nrow(data))
-#Set Seed so that same sample can be reproduced in future also
-#Always set the seed before sampling
-set.seed(101)
-#Now Selecting 70% of data as sample from 5000 rows of the data
-train_ind <- sample(seq_len(nrow(data)), size = sample_size)
-train <- data[train_ind, ]
-test <- data[-train_ind, ]
+
+# Begin multiple linear regression
 
 #Creating a multivariate linear model
 fit <- lm(price ~ ., data=train)
@@ -273,12 +274,15 @@ plot(fit)
 
 #Predicting house prices with the test dataset
 prediction <- predict.lm(fit, test)
+
 #Calculating the prediction error (real price - prediction)
 predError <- test$price - prediction
 
 #Calculating mean prediction error
 mean(abs(predError))
+
 #Getting highest prediction error
 max(abs(predError))
+
 #Getting smallest prediction error
 min(abs(predError))
